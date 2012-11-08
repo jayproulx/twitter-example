47053da8be243925b63041077f6a64faf9779964
Jay Proulx <jproulx@adobe.com>
2012-11-07 21:55:07 -0500

    Fixes #7
    JSONP requests don't gracefully fail.  Since they're supposed to register a call back, if the request fails, it's unlikely that the script block that executes the callback wasn't created, and so it fails silently.
    
    The easy option is to set a timeout, since in practice this request fails quickly, we can assume it has failed after abotu 2.5 seconds (probably less, but 2.5 seems safe).
    
    There's a jquery-jsonp plugin that can be used to add this functionality.
    
     https://github.com/jaubourg/jquery-jsonp

--- src/main/www/js/domain/User.js
+++ src/main/www/js/domain/User.js
@@ -1,7 +1,36 @@
-// TODO: When you enter an invalid handle, the error isn't reporting properly, it should be caught in .error(), but it's not, neither success nor error are called
 define ( ["use!backbone"],
 	function ( Backbone )
 	{
+		// Private Functions
+		// Functions in this closure, but not assigned to the object will not be visible outside of the closure.
+		// These AJAX response methods don't need to be accessed by external classes, so they don't need to be
+		// added to the domain object.
+
+		function authenticationSuccess ( user, data, status, xhr )
+		{
+			clearTimeout ( user.errorTimeout );
+			delete user.errorTimeout;
+
+			if ( data.length == 0 || xhr.status != 200 )
+			{
+				authenticationError ( user, xhr, status, "Received an invalid response from the server." );
+				return;
+			}
+
+			user.set ( data[0] );
+		}
+
+		function authenticationError ( user, xhr, status, error )
+		{
+			// TODO: This is a domain object, it shouldn't affect the view, it should dispatch an error event instead
+			// TODO: We should have a prettier notification, how about the modal bootstrap component?
+			alert ( "User not found for screen name @" + user.get ( "screenName" ) );
+
+			delete user.errorTimeout;
+
+			user.clear ();
+		}
+
 		return Backbone.Model.extend ( {
 			defaults: {
 				"screenName": ""
@@ -21,6 +50,15 @@ define ( ["use!backbone"],
 			{
 				var that = this;
 
+				// jsonp requests don't fail gracefully on a 404, we can't catch the error here, so we'll need to
+				// timeout instead of waiting for an error.  There's a way to work around this, but it's more work
+				// than would be ideal for this example.
+				// The ajax.error and deferred.fail methods aren't called when this request fails.
+				this.errorTimeout = setTimeout ( function ()
+				{
+					authenticationError ( that );
+				}, 2500 );
+
 				$.ajax ( {
 
 					url: this.url (),
@@ -29,24 +67,7 @@ define ( ["use!backbone"],
 
 					success: function ( data, status, xhr )
 					{
-						console.log ( "Fetched user profile" );
-
-						if ( data.length == 0 || xhr.status != 200 )
-						{
-							that.error ();
-							return;
-						}
-
-						that.set ( data[0] );
-					},
-
-					error: function ( xhr, status, error )
-					{
-						// TODO: This is a domain object, it shouldn't affect the view, it should dispatch an error event instead
-						// TODO: We should have a prettier notification, how about the modal bootstrap component?
-						alert ( "User not found" );
-
-						that.clear();
+						authenticationSuccess ( that, data, status, xhr );
 					}
 
 				} );
