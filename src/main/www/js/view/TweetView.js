baaae64324b87ce671e2c381d6f8d18e3c94cf25
Jay Proulx <jproulx@adobe.com>
2012-11-08 12:27:32 -0500

    Fixes #2
    Removed inline template (ugly) and pushed into the index file
    Added TweetViewTemplate to index files

--- src/main/www/js/view/TweetView.js
+++ src/main/www/js/view/TweetView.js
@@ -2,18 +2,10 @@ define ( ["jquery", "use!underscore", "use!backbone", "domain/Tweet"],
 
 	function ( $, _, Backbone, Tweet )
 	{
-		//TODO: Outsource this partial to an external file, this looks like junk
 		// Keeping the template here instead of in the prototype means that this only executes once, instead of
-		// every time we create a another view object
-		var tweetTemplate = _.template (
-			'<a class="pull-left" href="#">' +
-			'<img class="media-object" src="<%= profile_image_url %>">' +
-			'</a>' +
-			'<div class="media-body">' +
-			'<h4 class="media-heading"><%= from_user_name %> <small>@<%= from_user %></small></h4>' +
-			'<%= text %>' +
-			'</div>'
-		);
+		// every time we create a another view object.
+		// Using the body of an existing script element allows us to pre-load templates and reference them later
+		var tweetTemplate = _.template ( $ ( "#TweetViewTemplate" ).html () );
 
 		// Create a private index to help create a specific selector for the element this TweetView belongs to
 		var TWEET_INDEX = 0;
@@ -30,14 +22,14 @@ define ( ["jquery", "use!underscore", "use!backbone", "domain/Tweet"],
 				options = options || {};
 
 				// we shouldn't be missing a tweet object here, but if we are, lets use an empty one.
-				this.tweet = options.tweet || new Tweet();
+				this.tweet = options.tweet || new Tweet ();
 			},
 
-			render: function()
+			render: function ()
 			{
 				var json = this.tweet.toJSON ();
 
-				this.$el.empty();
+				this.$el.empty ();
 				this.$el.append ( tweetTemplate ( json ) );
 
 				return this.$el;
