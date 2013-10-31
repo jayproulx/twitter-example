da087c57547b85d0570857f8fb68e6c8d81853ba
Jay Proulx <jason.proulx@pollensoft.com>
2013-10-31 15:57:56 -0400

    Adding installation script to quickly download and install this repo on a Mac

---
+++ install-mac.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/pollensoft/twitter-example.git
+cd twitter-example
+git checkout feature/angularjs
+npm install
+bower install
+cp config.example.json config.json
+open config.json
+open https://dev.twitter.com/apps/new
