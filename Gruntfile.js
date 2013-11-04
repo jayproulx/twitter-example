e2d6234163290dce01a7f1ce78d2ef173f6dad12
Jay Proulx <jason.proulx@pollensoft.com>
2013-11-04 15:21:16 -0500

    Moved the aliasMappings config into browserify dev and dist, vendor was picking up all of the files in the defined aliasMapping.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,126 +1,128 @@
 module.exports = function (grunt) {
-    var startupError = false;
+	var startupError = false;
 
-    var config = {
-        www: {
-            files: 'src/main/www/js/**/*.js',
-            entry: 'src/main/www/dist/app.js',
-            scripts: 'src/main/www/js',
-        },
-        
-        vendor: {
-            files: [
+	var config = {
+		www: {
+			files: 'src/main/www/js/**/*.js',
+			entry: 'src/main/www/dist/app.js',
+            options: {
+                aliasMappings: {
+                    cwd: 'src/main/www/js',
+                    src: ['**/*.js'],
+                    dest: 'js'
+                }
+            }
+		},
+
+		vendor: {
+			files: [
                 "src/main/www/components/jquery/jquery.min.js",
                 "src/main/www/components/bootstrap/dist/js/bootstrap.min.js",
                 "src/main/www/components/angular/angular.min.js",
                 "src/main/www/components/angular-resource/angular-resource.min.js",
                 "src/main/www/components/angular-ui-router/release/angular-ui-router.js"
             ],
-            entry: "src/main/www/dist/vendor.js"
-        },
-        
-        node: {
-            files: 'src/main/node/**/*.js',
-            entry: 'src/main/node/server.js'
-        }
-    };
+			entry: "src/main/www/dist/vendor.js"
+		},
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+		node: {
+			files: 'src/main/node/**/*.js',
+			entry: 'src/main/node/server.js'
+		}
+	};
 
-        watch: {
-            express: {
-                files: config.node.files,
-                tasks: ['express:dev'],
-                options: {
-                    nospawn: true
-                }
-            },
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-            browserify: {
-                files: config.www.files,
-                tasks: ['browserify:dev']
-            }
-        },
+		watch: {
+			express: {
+				files: config.node.files,
+				tasks: ['express:dev'],
+				options: {
+					nospawn: true
+				}
+			},
 
-        browserify: {
-            vendor: {
-                src: config.vendor.files,
-                dest: config.vendor.entry,
-                options: {
-                    alias: [
+			browserify: {
+				files: config.www.files,
+				tasks: ['browserify:dev']
+			}
+		},
+
+		browserify: {
+			vendor: {
+				src: config.vendor.files,
+				dest: config.vendor.entry,
+				options: {
+					alias: [
                         "src/main/www/components/jquery/jquery.min.js:jquery",
                         "src/main/www/components/angular/angular.min.js:angular"
                     ],
-                    shim: {
-                        jquery: {
-                            path: 'src/main/www/components/jquery/jquery.min.js',
-                            exports: '$'
-                        },
-                        angular: {
-                            path: 'src/main/www/components/angular/angular.min.js',
-                            exports: 'angular'
-                        }
-                    },
-                    transform: ['uglifyify']
-                }
-            },
+					shim: {
+						jquery: {
+							path: 'src/main/www/components/jquery/jquery.min.js',
+							exports: '$'
+						},
+						angular: {
+							path: 'src/main/www/components/angular/angular.min.js',
+							exports: 'angular'
+						}
+					},
+					transform: ['uglifyify']
+				}
+			},
 
-            dev: {
-                src: config.www.files,
-                dest: config.www.entry
-            },
+			dev: {
+				src: config.www.files,
+				dest: config.www.entry,
+				options: {
+					aliasMappings: config.www.options.aliasMappings
+				}
+			},
 
-            dist: {
-                src: config.www.files,
-                dest: config.www.entry,
-                options: {
-                    transform: ['uglifyify']
-                }
-            },
+			dist: {
+				src: config.www.files,
+				dest: config.www.entry,
+				options: {
+					transform: ['uglifyify'],
+					aliasMappings: config.www.options.aliasMappings
+				}
+			}
+		},
 
-            options: {
-                aliasMappings: {
-                    cwd: config.www.scripts,
-                    src: ['**/*.js'],
-                    dest: 'js'
-                }
-            }
-        },
+		express: {
+			options: {
+				error: function (err, result, code) {
+					console.log(err, result, code);
+					if (code === 3) {
+						startupError = true;
+						grunt.fail.warn('Grunt had to stop because we encountered an error starting up express.  This probably means you need to CTRL-C to quit watch.', 1);
+					}
+				}
+			},
 
-        express: {
-            options: {
-                error: function (err, result, code) {
-                    console.log(err, result, code);
-                    if (code === 3) {
-                        startupError = true;
-                        grunt.fail.warn('Grunt had to stop because we encountered an error starting up express.  This probably means you need to CTRL-C to quit watch.', 1);
-                    }
-                }
-            },
+			dev: {
+				options: {
+					script: config.node.entry,
+					background: true
+				}
+			},
 
-            dev: {
-                options: {
-                    script: config.node.entry,
-                    background: true
-                }
-            },
+			dist: {
+				options: {
+					script: config.node.entry,
+					background: true
+				}
+			}
+		}
+	});
 
-            dist: {
-                options: {
-                    script: config.node.entry,
-                    background: true
-                }
-            }
-        }
-    });
+	grunt.loadNpmTasks('grunt-express-server');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.loadNpmTasks('grunt-express-server');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-browserify');
-
-    grunt.registerTask('server', ['express:dev', 'browserify:vendor', 'browserify:dev', 'watch']);
-    grunt.registerTask('server:dev', 'server');
-    grunt.registerTask('server:dist', ['express:dist', 'browserify:vendor', 'browserify:dist']);
-    grunt.registerTask('travisci', 'browserify');
+	grunt.registerTask('server', ['express:dev', 'browserify:vendor', 'browserify:dev', 'watch']);
+	grunt.registerTask('server:dev', 'server');
+	grunt.registerTask('server:dist', ['express:dist', 'browserify:vendor', 'browserify:dist']);
+	grunt.registerTask('travisci', 'browserify');
 };
